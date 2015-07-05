e0a267b0dfe8f2fbff3d590083d89277ed2e50f0
MaryGorshkova <mint.mushrooom@gmail.com>
2015-07-06 02:09:23 +0300

    some dirty hacks for sweet dreams

--- www/script/script.js
+++ www/script/script.js
@@ -7,6 +7,16 @@ function Show(){
 	if ((window.innerWidth-1150) > 170){
 		newColumn();
 	}
+	
+	if (window.innerHeight > 600){
+		var h = window.innerHeight - 350;	
+		var data = document.getElementById('data');
+		data.style.height = h.toString() + "px";
+	}
+	else{
+		var body = document.getElementById('body');
+		body.style.overflowY = "scroll";
+	}
 }
 
 function newColumn(){
@@ -26,4 +36,13 @@ function newColumn(){
 	var row02 = document.getElementById("data");
 	row02.insertBefore(newElem, row02.childNodes[4]);
 
+	var pages = document.getElementById("pages");
+	pages.style.width = "1238px";
+	pages.innerHTML = '<em>←</em>' +
+						'<span>01-02<div class="line"></div></span><span class="space"></span>' +
+						'<span class="selected">03-05<div class="line"></div></span><span class="space"></span>' +
+						'<span>06-08<div class="line"></div></span><span class="space"></span>' +
+						'<span>9-11<div class="line"></div></span><span class="space"></span>' +
+						'<span>12<div class="line"></div></span><span class="space"></span>' +
+						'<em>→</em>';
 }
